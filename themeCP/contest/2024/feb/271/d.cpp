d6b49de0ca0ecd555b45e47c28a195abd8a4ad46
Manueljunio <manueljunioferraz@hotmail.com>
2025-03-01 00:24:33 -0300

    ?

--- themeCP/contest/2024/feb/271/d.cpp
+++ themeCP/contest/2024/feb/271/d.cpp
@@ -9,7 +9,23 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 void solve() {
-    
+    ll n,a,b;
+    cin >> n >> a >> b;
+    a=gcd(a,b);
+
+    vector<ll> v(n);
+    for(auto &i:v) {
+        cin >> i;
+        i%=a;
+    }
+
+    sort(v.begin(),v.end());
+
+    ll m = v.back()-v.front();
+
+    for(ll j=1;j<n;j++) m=min(m,v[j-1]+a-v[j]);
+
+    cout << m << '\n';
 }
 
 int main() { _
