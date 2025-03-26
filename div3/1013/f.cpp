75ca223f11b9d6298f53dac4c598de2ef2a5c20a
Manueljunio <manueljunioferraz@hotmail.com>
2025-03-26 12:52:06 -0300

    fixed f(div3)

--- div3/1013/f.cpp
+++ div3/1013/f.cpp
@@ -3,78 +3,59 @@
 using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define sobe first
-#define lado second
+#define ff first
+#define ss second
 typedef long long ll;
 typedef unsigned long long ull;
 
 const ll mod = 998244353;
 ll n,m,d;
 
-vector<string> v;
 
-vector<vector<pair<ll,ll>>> dp;
-
-ll calc(int x,int y);
-
-ll calcs(int x,int y) {
-    if(v[x][y]=='#') return 0;
-    if(dp[x][y].sobe!=-1) return (dp[x][y].sobe)%mod;
-    if(x==n-1) return dp[x][y].sobe=1;
-    dp[x][y].sobe = 0;
-    for(ll i=0;i<m;i++) {
-        if((d*d)-1>((i-x)*(i-x)) and i!=y) dp[x][y].sobe = (dp[x][y].sobe+calc(x+1,i))%mod;
-    }
-    // cout << x << ' ' << y << ' ' << dp[x][y].sobe << '\n';
-
-    return dp[x][y].sobe%mod;
-}
-
-ll calc(int x, int y) {
-    if(v[x][y]=='#') return 0;
-    if(dp[x][y].sobe!=-1 and dp[x][y].lado!=-1) return (dp[x][y].sobe+dp[x][y].lado)%mod;
-    if(x==n-1) {
-        if(dp[x][y].sobe==-1) dp[x][y].sobe=1;
-        if(dp[x][y].lado==-1) {
-            dp[x][y].lado=0;
-            for(int i=max(y-d,0LL);i<=min(m-1,y+d);i++) {
-                if(i==y) continue;
-                if(v[x][i]=='X') dp[x][y].lado++;
-            }
-        }
-        // cout << x << ' ' << y << ' ' << dp[x][y].sobe+dp[x][y].lado << '\n';
-        return (dp[x][y].sobe+dp[x][y].lado)%mod;
-    }
-
-    // linha de cima
-    if(dp[x][y].sobe==-1) {
-        dp[x][y].sobe = 0;
-        for(int i=max(0LL,y-d+1);i<=min(m-1,y+d-1);i++) {
-            auto ababa = (y-i)*(y-i)+1;
-            if(d*d>=ababa*ababa) dp[x][y].sobe+=calc(x+1,i);
-        }
-    }
-    if(dp[x][y].lado==-1){
-        dp[x][y].lado = 0;
-        for(ll i=max(y-d,0LL);i<=min(m-1LL,y+d);i++) if(i!=y) dp[x][y].lado+=calcs(x,i);
-    }
-
-    dp[x][y].sobe%=mod;
-    dp[x][y].lado%=mod;
-    // cout << x << ' ' << y << ' ' << dp[x][y].sobe+dp[x][y].lado << '\n';
-    return (dp[x][y].sobe+dp[x][y].lado)%mod;
+void make_sp(vector<ll> &v,vector<ll> &sp) {
+    sp[0]=v[0];
+    for(ull i=1;i<v.size();i++) sp[i] = sp[i-1]+v[i];
 }
 
 void solve() {
     cin >> n >> m >> d;
-    v.resize(n);
-    dp.assign(n,vector<pair<ll,ll>>(m,make_pair(-1,-1)));
-    for(auto s = v.rbegin();s!=v.rend();s++) cin >> *s;
+    vector<string> v(n);
+    for(ll i=0;i<n;i++) cin >> v[i];
     
-    ll ans=0;
-    for(int i=0;i<m;i++) ans=(ans+calc(0,i))%mod;
-    cout << ans%mod << '\n';
-    // cout << dp[2][1].lado << '\n';
+    vector<ll> v1(m,0),sp(m,0);
+    for(ll i=0;i<m;i++) if(v[0][i]=='X') v1[i]++;
+    make_sp(v1,sp);
+    
+    for(ll i=0;i<m;i++) {
+        if(v[0][i]=='#') continue;
+        if(i) {
+            if(i-d>0) v1[i]+=sp[i-1]-sp[i-d-1];
+            else v1[i]+=sp[i-1];
+        }
+        v1[i]+=sp[min(m-1,i+d)]-sp[i];
+    }
+    
+    make_sp(v1,sp);
+    for(ll r=1;r<n;r++) {
+        for(ll i=0;i<m;i++) {
+            if(v[r][i]=='#') v1[i] = 0;
+            else if(i-d>=0) v1[i] = (sp[min(m-1,i+d-1)]-sp[i-d])%mod;
+            else v1[i] = sp[min(m-1,i+d-1)]%mod;
+        } 
+
+        make_sp(v1,sp);
+        for(ll i=0;i<m;i++) {
+            if(v[r][i]=='#') continue;
+            if(i) {
+                if(i-d>0) v1[i]=(v1[i]+sp[i-1]-sp[i-d-1])%mod;
+                else v1[i]=(v1[i]+sp[i-1])%mod;
+            }
+            v1[i]=(v1[i]+sp[min(m-1,i+d)]-sp[i])%mod;
+        }
+        make_sp(v1,sp);
+    }
+
+    cout << sp.back()%mod << '\n';
 }
 
 int main() {
