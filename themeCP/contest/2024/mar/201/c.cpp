78cade8a37cf18a1408f6696a7a4eae660d9d3c6
manuelferrazc <manueljunioferraz@hotmail.com>
2025-03-31 16:29:57 -0300

    ababa

--- themeCP/contest/2024/mar/201/c.cpp
+++ themeCP/contest/2024/mar/201/c.cpp
@@ -10,18 +10,32 @@ typedef unsigned long long ull;
 
 set<ll> s;
 
+bool check(ll n) {
+    ll l=2,r=1'000'000'000LL;
+
+    while(l<=r) {
+        ll m = (l+r)>>1LL;
+        ll c = 1LL+m+m*m;
+        if(c==n) return true;
+        else if(c<n) l = m+1;
+        else r=m-1;
+    }
+    return false;
+}
+
 void solve() {
     ll n;
     cin >> n;
 
-    if(s.count(n)) cout << "yeS\n";
-    else cout << "nO\n";
+    if(s.count(n) or check(n)) cout << "YES\n";
+    else cout << "NO\n";
 }
 
 int main() { _
-    for(ll k=2;k<=1'000'000LL;k++) {
-        ll sum = k+1,p=k;
-        while(sum+p*k<=1'000'000LL) {
+    for(ll k=2;k<=1'000'000;k++) {
+        ll sum = 1+k+k*k,p=k*k;
+        ll lim = 1'000'000'000'000'000'000LL;
+        while(sum<lim and k<=(lim-sum)/p) {
             p*=k;
             sum+=p;
             s.insert(sum);
