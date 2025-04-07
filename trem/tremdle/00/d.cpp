862d4c54ca8d549b7af0672ed8537860c0fb597b
manuelferrazc <manueljunioferraz@hotmail.com>
2025-04-07 14:41:25 -0300

    4/4

--- trem/tremdle/00/d.cpp
+++ trem/tremdle/00/d.cpp
@@ -8,13 +8,40 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve() {
-    
+ll n,m;
+set<pair<ll,ll>> s;
+
+bool test(ll x) {
+    for(pair<ll,ll> p:s) {
+        ll a = (p.ff+x)%n;
+        ll b = (p.ss+x)%n;
+        if(not s.count(make_pair(min(a,b),max(a,b))))return false;
+    }
+    return true;
 }
 
 int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+    cin >> n >> m;
+    vector<ll> d;
+    for(ll i=1;i<n;i++) if(n%i==0) d.push_back(i);
+
+    for(ll i=0;i<m;i++) {
+        ll a,b;
+        cin >> a >> b;
+        a--;
+        b--;
+        
+        s.insert(make_pair(min(b,a),max(a,b)));
+    }
+
+    for(ll div:d) {
+        if(test(div)) {
+            cout << "Yes\n";
+            return 0;
+        }
+    }
+
+    cout << "No\n";
+
     return 0;
 }
