f49fd653252fe2d329a7cfda23d48e7b3e90fdc5
manuelferrazc <manueljunioferraz@hotmail.com>
2025-04-14 16:57:28 -0300

    ababa

--- trem/tremdle/7/c.cpp
+++ trem/tremdle/7/c.cpp
@@ -13,8 +13,20 @@ void solve() {
 }
 
 int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+    ll n;
+    cin >> n;
+    map<ll,ll> m,m2;
+    while(n--) {
+        ll a,b;
+        cin >> a >> b;
+        m[a]++;
+        m2[b]++;
+    }
+
+    n=0;
+    ll sla=0;
+    for(auto p:m) n+=p.ss-1;
+    for(auto p:m2) sla+=p.ss-1;
+    cout << min(sla*sla,n*n) << '\n';
     return 0;
 }
---
+++ trem/tremdle/8/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve() {
+    
+}
+
+int main() {
+    cout << "? 6\n";
+    cout.flush();
+    ll a;
+    cin >> a;
+    cout << "! " << (a^6LL) << '\n';
+    cout.flush();
+    return 0;
+}
