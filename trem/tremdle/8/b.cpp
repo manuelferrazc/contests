e0de39bfb173bb4557ec801722f2c788fdf8e158
Manueljunio <manueljunioferraz@hotmail.com>
2025-04-15 12:11:12 -0300

    ababa

--- trem/tremdle/8/b.cpp
+++ trem/tremdle/8/b.cpp
@@ -8,13 +8,61 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve() {
+vector<vector<int>> adj(1001);
+set<int> ans, q;
+int tg, cnt;
+
+void dfs(int v, int p=-1) {
+    if(tg==cnt) return;
+
+    q.insert(v);
+    if(ans.count(v)) cnt++;
     
+    for(int u:adj[v]) {
+        if(u==p) continue;
+        dfs(u,v);
+    }
 }
 
-int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++) ans.insert(i+1);
+
+    for(int emaneru=1;emaneru<n;emaneru++) {
+        int gg, cabessa; // sla kkk
+        cin >> gg >> cabessa;
+        adj[gg].push_back(cabessa);
+        adj[cabessa].push_back(gg);
+    }
+
+    while(ans.size()>1ULL) {
+        tg = ans.size()/2;
+        cnt=0;
+        q.clear();
+
+        dfs(1);
+        cout << "? " << q.size() << ' ';
+        for(int i:q) cout << i << ' ';
+        cout << '\n';
+        cout.flush();
+        
+        int x;
+        cin >> x;
+        if(x>=0) {
+            auto it = ans.begin();
+            while(it!=ans.end()) {
+                if(((int)q.count(*it))!=x) {
+                    int e = *it;
+                    it++;
+                    ans.erase(e);
+                } else it++;
+            }
+        } else return 0;
+    }
+
+    cout << "! " << *ans.begin() << '\n';
+    cout.flush();
+
     return 0;
 }
--- trem/tremdle/8/c.cpp
+++ trem/tremdle/8/c.cpp
@@ -8,13 +8,91 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve() {
-    
+ll count(ll n) {
+    ll r=0;
+    for(ll i=0;i<63;i++) if(n&(1LL<<i)) r++;
+    return r;
 }
 
-int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+int main() {
+    
+    cout << "? 0\n";
+    cout.flush();
+    ll qtd, ans=0;
+    cin >> qtd;
+    srand(qtd);
+    qtd = count(qtd);
+
+    vector<ll> erro;
+    for(ll i=0;i<59;i+=2) {
+        ll q=0, x;
+        q|=1LL<<i;
+        q|=1LL<<(i+1);
+        cout << "? " << q << '\n';
+        cout.flush();
+        cin >> x;
+        x = count(x);
+        if(x==qtd+2) continue; // os bits q perguntei são 0
+        else if(x==qtd-2) { // os bits q perguntei são 1
+            ans|=1LL<<i;
+            ans|=1LL<<(i+1);
+        } else erro.push_back(i); // os bits q perguntei são 01 ou 10
+    }
+
+    while(erro.size()) {
+        if(erro.size()==1) {
+            ll q=1LL<<erro.front();
+            cout << "? " << q << '\n';
+            cout.flush();
+            cin >> q;
+            if(q==-1LL) return 0;
+            if(count(q)==qtd+1) ans|=1LL<<(erro.front()+1);
+            else ans|=1LL<<(erro.front());
+            erro.pop_back();
+            continue;
+        }
+
+        ll b1 = erro.back();
+        erro.pop_back();
+        ll b2 = erro.back(),r1=rand()&1,r2=rand()&1;
+        erro.pop_back();
+
+    
+        ll q = 1LL<<(b1+r1);
+        q |= 1LL<<(b2+r2);
+
+        cout << "? " << q << '\n';
+        cout.flush();
+
+        ll res, qq;
+        cin >> res;
+        if(res==-1LL) return 0;
+        qq = count(res);
+        if(qq==qtd+2) {
+            ans|=1LL<<(b1+(r1==0LL));
+            ans|=1LL<<(b2+(r2==0LL));
+        } else if(qq==qtd-2) {
+            ans|=1LL<<(b1+r1);
+            ans|=1LL<<(b2+r2);
+        } else { // acertei só 1, tem q perguntar dnv :(
+            q = 1LL<<(b1+r1);
+            cout << "? " << q << '\n';
+            cout.flush();
+            cin >> q;
+            if(q==-1LL) return 0;
+            q = count(q);
+            if(q==qtd+1) { // acertei um 0 no primeiro par, logo o do segundo par ta 1
+                ans|=1LL<<(b1+(r1==0LL));
+                ans|=1LL<<(b2+r2);
+            } else { // cc
+                ans|=1LL<<(b1+r1);
+                ans|=1LL<<(b2+(r2==0LL));
+            }
+        }
+    }
+
+    cout << "! " << ans << '\n';
+    cout.flush();
+
     return 0;
 }
