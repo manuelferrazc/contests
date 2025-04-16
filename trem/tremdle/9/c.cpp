c41a03fced3fb95cedc867dd869215c3d64f24c9
Manueljunio <manueljunioferraz@hotmail.com>
2025-04-16 09:24:40 -0300

    ababa

--- trem/tremdle/9/c.cpp
+++ trem/tremdle/9/c.cpp
@@ -12,9 +12,46 @@ void solve() {
     
 }
 
-int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    int um=1;
+    for(int i=2;i<=n;i++) {
+        cout << "? " << um << ' ' << um << ' ' << i << '\n';
+        cout.flush();
+        cin >> s;
+        if(s[0]=='Y') um = i;
+    }
+
+    vector<int> v;
+    v.push_back(1);
+
+    for(int i=2;i<=n;i++) {
+        int l = 0, r = v.size();
+
+        while(l<r) {
+            int m = (l+r)>>1;
+            cout << "? " << um << ' ' << i << ' ' << v[m] << '\n';
+            cout.flush();
+            cin >> s;
+            if(s[0]=='-') return 0;
+            else if(s[0]=='Y') l=m+1;
+            else r=m;
+        }
+
+        v.insert(v.begin()+r,i);
+    }
+
+    cout << "! ";
+    for(int i=1;i<=n;i++) {
+        for(ull j=0;j<v.size();j++) {
+            if(v[j]==i) {
+                cout << j+1 << ' ';
+                break;
+            }
+        }
+    }
+    cout << '\n';
     return 0;
 }
--- trem/tremdle/9/d.cpp
+++ trem/tremdle/9/d.cpp
@@ -8,13 +8,26 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve() {
-    
+const ll mod = 998244353LL;
+
+ll fexp(ll n, ll e) {
+    if(e==1) return n;
+    if(e==0) return 1;
+
+    ll r= fexp(n,e>>1);
+    r = r*r%mod;
+    if(e&1) r = r*n%mod;
+    return r;
 }
 
 int main() { _
-    int t;
-    cin >> t;
-    while(t--) solve();
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int o=0;
+    for(char c:s) if(c=='1') o++;
+
+    cout << fexp(2,2*n)  << '\n';
     return 0;
 }
