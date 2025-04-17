8350cb1ebd72555a1d765a3223483047cf34fb3f
manuelferrazc <manueljunioferraz@hotmail.com>
2025-04-17 17:04:06 -0300

    ababa

--- div3/1016/d.cpp
+++ div3/1016/d.cpp
@@ -17,41 +17,40 @@ ll find_number(ll x1,ll x2,ll y1,ll y2) {
         if(x==2 and y==1) return 3;
         return 4;
     }
-
+    // cout << x << ' ' << y << '\n';
+    // cout << x2 << ' ' << y2 << '\n' << '\n';
     if(x>x2/2 and y>y2/2) {
         x-=x2/2;
         y-=y2/2;
-        return x2+find_number(x1,x2/2,y1,y2/2);
+        return y2*y2/4+find_number(x1,x2/2,y1,y2/2);
     } else if(x>x2/2) {
         x-=x2/2;
-        return x2*2+find_number(x1,x2/2,y1,y2/2);
+        return y2*y2/4*2+find_number(x1,x2/2,y1,y2/2);
     } else if(y>y2/2) {
         y-=y2/2;
-        return y2*3+find_number(x1,x2/2,y1,y2/2);
+        return y2*y2/4*3+find_number(x1,x2/2,y1,y2/2);
     } else return find_number(x1,x2/2,y1,y2/2);
 }
 
 pair<ll,ll> find_pos(ll v, ll p) {
-    if(p==1){
-        if(v==1) return {1,1};
-        if(v==2) return {2,2};
-        if(v==3) return {2,1};
-        if(v==4) return {1,2};
-    }
+    if(v==1) return {1,1};
+    if(v==2) return {2,2};
+    if(v==3) return {2,1};
+    if(v==4) return {1,2};
 
-    if(v>(1LL<<p) and v<=(1LL<<(p+1))) {
-        v-=1LL<<p;
+    if(v>(1LL<<(2*p-2)) and v<=(1LL<<(2*p-1))) {
+        v-=1LL<<(p*2-2);
         auto c = find_pos(v,p-1);
         c.ff+=1LL<<(p-1);
         c.ss+=1LL<<(p-1);
         return c;
-    } else if(v>(1LL<<(p+1)) and v<=(3*(1LL<<p))) {
-        v-=1LL<<(p+1);
+    } else if(v>((1LL<<(2*p-1))) and v<=((1ll<<(2*p-1))+(1LL<<(2*p-2)))) {
+        v-=(1LL<<(2*p-1));
         auto c = find_pos(v,p-1);
         c.ff+=1LL<<(p-1);
         return c;
-    } else if(v>(3*(1LL<<p))) {
-        v-=(3*(1LL<<p));
+    } else if(v>((1ll<<(2*p-1))+(1LL<<(2*p-2)))) {
+        v-=((1ll<<(2*p-1))+(1LL<<(2*p-2)));
         auto c = find_pos(v,p-1);
         c.ss+=1LL<<(p-1);
         return c;
@@ -68,14 +67,14 @@ void solve() {
             cin >> y;
             cout << find_number(1,1LL<<n,1,1LL<<n) << '\n';
         } else {
-            auto p = find_pos(x,n);
+            auto p = find_pos(x,n+1);
             cout << p.ff << ' ' << p.ss << '\n';
         }
     }
 
 }
 
-int main() { _
+int main() { 
     int t;
     cin >> t;
     while(t--) solve();
