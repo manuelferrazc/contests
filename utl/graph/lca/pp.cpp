79577a33846b2e0263f85f2ba02044a90cdf5c63
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-03 12:00:21 -0300

    sla man

---
+++ utl/graph/lca/pp.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+
+vector<vector<int>> adj,ka;
+vector<int> d;
+int l=0;
+
+void dfs(int v, int p=-1) {
+    if(p!=-1) d[v] = d[p]+1;
+    ka[v][0] = p;
+
+    for(int i=1;i<l;i++) {
+        if(ka[v][i-1]!=-1) ka[v][i] = ka[ ka[v][i-1] ][i-1];
+    }
+
+    for(int u:adj[v]) {
+        if(u==p) continue;
+        dfs(u,v);
+    }
+}
+
+int getKA(int a, int k) {
+    for(int i=0;i<l;i++) if(k&(1<<i)) a = ka[a][i];
+    return a;
+}
+
+int getLCA(int a, int b) {
+    if(d[a]<d[b]) swap(a,b);
+    if(d[a]!=d[b]) a = getKA(a,d[a]-d[b]);
+    if(a==b) return a;
+    
+    for(int i=l-1;i>=0;i--) {
+        if(ka[a][i]!=ka[b][i]) {
+            a = ka[a][i];
+            b = ka[b][i];
+        }
+    }
+
+    return ka[a][0];
+}
+
+int main() { _
+    int n,q;
+    cin >> n;
+
+    for(int i=0;i<30;i++) if(n&(1<<i)) l=i+1;
+    adj.resize(n);
+    ka.assign(n,vector<int>(l,-1));
+    d.resize(n);
+    d[0] = 0;
+
+    for(int i=1;i<n;i++) {
+        int a,b;
+        cin >> a >> b;
+        a--;
+        b--;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    dfs(0);
+
+    cin >> q;
+    while(q--) {
+        int k;
+        cin >> k;
+        vector<int> c(k);
+        for(int &i:c) {
+            cin >> i;
+            i--;
+        }
+
+        int mi = c[0];
+        for(int i=1;i<k;i++) if(d[c[i]]>d[mi]) mi = c[i];
+
+        int m2 = -1;
+        for(int i=0;i<k;i++) {
+            if(getLCA(mi,c[i])!=c[i]) {
+                m2 = i;
+                break;
+            }
+        }
+        if(m2==-1) {
+            cout << "YES\n";
+            continue;
+        }
+
+        for(int i = m2+1;i<k;i++) 
+            if(getLCA(c[i],c[m2])==c[m2]) m2 = i;
+        
+        int anc = getLCA(mi,c[m2]);
+        bool ok=true;
+        for(int i=0;i<k;i++) {
+            if(d[c[i]]<d[anc]) {
+                ok = false;
+                break;
+            }
+            if(getLCA(mi,c[i])!=c[i] and getLCA(c[i],c[m2])!=c[i]) {
+                ok = false;
+                break;
+            }
+        }
+
+        if(ok) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
