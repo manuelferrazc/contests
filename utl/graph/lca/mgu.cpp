b96021a14c7083ff524e883d0206c9aec792f8de
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-03 15:10:34 -0300

    im not tankando anymore

--- utl/graph/lca/mgu.cpp
+++ utl/graph/lca/mgu.cpp
@@ -57,45 +57,28 @@ int calc(int a, int b, int f) {
     if(com==a) {
         if(fb==f) return 1;
         if(fb==b) return d[f]-d[b]+1;
-        // if(fb==a) return d[f]-d[a]+1;
         return d[f]+1-d[fb];
     }
     if(com==b) {
         if(fa==f) return 1;
         if(fa==a) return d[f]-d[a]+1;
-        // if(fb==b) return d[f]-d[b]+1;
         return d[f]+1-d[fa];
     }
-    // tem um ancestral diferente
-    if(fa==f or fb==f) return 1;
-    if(fa==a) return 1+d[f]-d[a];
-    if(fb==b) return 1+d[f]-d[b];
-    return d[ab]-2*d[com]+d[f]+1;
-    // if(ab==f and ab!=a and ab!=b) return d[a]+d[b]-2*d[f]+1;
-    // if(fb==a) return d[b]+d[f]-2*d[a]+1;
-    // if(fa==b) return d[a]+d[f]-2*d[b]+1;
-    // if(com==f) {
 
-    // }
-    
-    
-    // if(com==ab and ((fa==f) != (fb==f))) return 1;
-    // if(ab==a and fb==b) return 1+d[f]-d[b];
-    // if(ab==b and fa==a) return 1+d[f]-d[a];
-    // if(fa==f and ab==a) return 1+d[a]-d[f];
-    // if(fa==f and ab==b) return 1+d[b]-d[f];
-    // if(com==ab) return d[f]-d[ab]+1;
-    // if(fa==a and fb==b) return d[f] - max(d[a],d[b]) + 1;
-    // if(fa==a) return d[f]-d[a]+1;
-    // if(fb==b) return d[f]-d[b]+1;
-    // else return d[ab]-2*d[com]+d[f]+1;
+    if(com==fa and fa==fb and fb==ab) return d[f]-d[com]+1;
+    if(fa==f or fb==f) return 1;
+    if(fa==a) return d[f]-d[a]+1;
+    if(fb==b) return d[f]-d[b]+1;
+    if(ab!=com) return d[ab]-2*d[com]+d[f]+1;
+    if(d[fa]>d[fb]) return d[f]-d[fa]+1;
+    return d[f]-d[fb]+1;
 }
 
 int main() { _
     int n,q;
     cin >> n >> q;
 
-    for(int i=0;i<30;i++) if(n&(1<<i)) l=i;
+    for(int i=0;i<30;i++) if(n&(1<<i)) l=i+1;
     adj.resize(n);
     ka.assign(n,vector<int>(l,-1));
     d.resize(n);
@@ -120,9 +103,6 @@ int main() { _
         c--;
 
         int ans = max({calc(a,b,c),calc(a,c,b),calc(b,c,a)});
-        // cout << a+1 << b+1 << c+1 << ": " << calc(a,b,c) << '\n';
-        // cout << a+1 << c+1 << b+1 << ": " << calc(a,c,b) << '\n';
-        // cout << b+1 << c+1 << a+1 << ": " << calc(b,c,a) << '\n';
         cout << ans << '\n';
     }
 
