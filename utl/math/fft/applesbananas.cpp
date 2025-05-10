cfc884d3866e25ceb60f361438faa3f734a4efd4
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-09 23:55:14 -0300

    ababa

---
+++ utl/math/fft/applesbananas.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    ll n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (ll i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (ll i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+vector<ll> multiply(vector<ll> const& a, vector<ll> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    ll n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (ll i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<ll> result(n);
+    for (ll i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+int main() { _
+    ll n,m,k;
+    cin >> k >> n >> m;
+	
+    vector<ll> a(k+1,0),b(k+1,0);
+	for(ll i=0;i<n;i++) {
+		ll x;
+		cin >> x;
+		a[x]+=1;
+	}
+
+	for(ll i=0;i<m;i++) {
+		ll x;
+		cin >> x;
+		b[x]+=1;
+	}
+
+	auto ans = multiply(a,b);
+	k<<=1;
+	for(ll i=2;i<=k;i++) cout << ans[i] << ' ';
+    cout << '\n';
+    return 0;
+}   
