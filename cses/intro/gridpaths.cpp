5cff05c216528b48ef860e4a3ea5dacca72d2aad
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-15 23:14:03 -0300

    sudo apt get-install git

--- cses/intro/gridpaths.cpp
+++ cses/intro/gridpaths.cpp
@@ -11,34 +11,35 @@ typedef unsigned long long ull;
 int main() { _
     string s;
     cin >> s;
+    s = '1' + s;
     
-    vector<vector<vector<ull>>> dp(7,vector<vector<ull>>(7,vector<ull>(48,0)));
-    if(s.back()=='D' or s.back()=='?') dp[5][6][47] = 1;
-    if(s.back()=='L' or s.back()=='?') dp[6][5][47] = 1;
+    vector<vector<vector<ull>>> dp(7,vector<vector<ull>>(7,vector<ull>(49,0)));
+    dp[0][0][0] = 1;
 
-    for(int t = 46;t>=0;t--) {
+    // cout << dp[5][6][47] << '\n';
+    for(int t = 1;t<=48;t++) {
         for(int x=0;x<7;x++) {
             for(int y=0;y<7;y++) {
                 if(s[t]=='U') {
-                    if(x) dp[x][y][t] = dp[x-1][y][t+1];
+                    if(x!=6) dp[x][y][t] += dp[x+1][y][t-1];
                 } else if(s[t]=='D') {
-                    if(x!=6) dp[x][y][t] = dp[x+1][y][t+1];
+                    if(x) dp[x][y][t] += dp[x-1][y][t-1];
                 } else if(s[t]=='L') {
-                    if(y) dp[x][y][t] = dp[x][y-1][t+1];
+                    if(y!=6) dp[x][y][t] += dp[x][y+1][t-1];
                 } else if(s[t]=='R') {
-                    if(y!=6) dp[x][y][t] = dp[x][y+1][t+1];
+                    if(y) dp[x][y][t] += dp[x][y-1][t-1];
                 } else {
-                    if(x) dp[x][y][t] += dp[x-1][y][t+1];
-                    if(x!=6) dp[x][y][t] += dp[x+1][y][t+1];
-                    if(y) dp[x][y][t] += dp[x][y-1][t+1];
-                    if(y!=6) dp[x][y][t] += dp[x][y+1][t+1];
+                    if(x!=6) dp[x][y][t] += dp[x+1][y][t-1];
+                    if(x) dp[x][y][t] += dp[x-1][y][t-1];
+                    if(y!=6) dp[x][y][t] += dp[x][y+1][t-1];
+                    if(y) dp[x][y][t] += dp[x][y-1][t-1];
                 }
             }
         }
     }
     
 
-    cout << dp[0][0][0] << '\n';
+    cout << dp[6][0][48] << '\n';
     
     return 0;
 }
