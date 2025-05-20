6aaa55c2a3f70b2310dc4d9a892071e3129456d7
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-20 11:56:36 -0300

    but youre not

--- cses/sortsearch/movie2.cpp
+++ cses/sortsearch/movie2.cpp
@@ -9,6 +9,28 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 int main() { _
+    int n, k, ans=0;
+    cin >> n >> k;
+    vector<pair<int,int>> v(n);
+
+    for(auto &i:v) cin >> i.ff >> i.ss;
+    sort(v.begin(),v.end());
+
+    priority_queue<int,vector<int>,greater<int>> pq;
+    for(int i=0;i<k;i++) {
+        ans++;
+        pq.push(v[i].ss);
+    }
+
+    for(int i=k;i<n;i++) {
+        if(pq.top()<v[i].ff) {
+            ans++;
+            pq.pop();
+            pq.push(v[i].ss);
+        }
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
