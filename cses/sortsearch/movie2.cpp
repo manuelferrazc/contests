0f3d755c2443e46c5301721b043e36fec9ffb0d0
Manueljunio <manueljunioferraz@hotmail.com>
2025-05-19 23:46:56 -0300

    a picture

---
+++ cses/sortsearch/movie2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ss first
+#define ff second
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() { _
+
+    return 0;
+}
