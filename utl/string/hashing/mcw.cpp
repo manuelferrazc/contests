ce75746759b31fd06966dcb3750d8ddf3c0b03ce
manuelferrazc <manueljunioferraz@hotmail.com>
2025-07-10 17:02:46 -0300

    hashh

--- utl/string/hashing/mcw.cpp
+++ utl/string/hashing/mcw.cpp
@@ -9,9 +9,32 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 const ll mod = 1'000'000'007LL;
-const ll p = 200'003;
+ll b = 200'003;
 
-vector<ll> pot;
+ll pot = 1;
+
+mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
+ 
+int uniform(int l, int r) {
+	uniform_int_distribution<int> uid(l, r);
+	return uid(rng);
+}
+ 
+template<int MOD> struct str_hash { // 116fcb
+	static ll P;
+	vector<ll> h, p;
+	str_hash(vector<ll> &s) : h(s.size()), p(s.size()) {
+        b = P;
+		p[0] = 1, h[0] = s[0];
+		for (int i = 1; i < s.size(); i++)
+			p[i] = p[i - 1]*P%MOD, h[i] = (h[i - 1]*P + s[i])%MOD;
+	}
+	ll operator()(int l, int r) { // retorna hash s[l...r]
+		ll hash = h[r] - (l ? h[l - 1]*p[r - l + 1]%MOD : 0);
+		return hash < 0 ? hash + MOD : hash;
+	}
+};
+template<int MOD> ll str_hash<MOD>::P = uniform(256, MOD - 1);
 
 ll fexp(ll n, ll e) {
     if(e==0) return 1;
@@ -24,49 +47,29 @@ ll fexp(ll n, ll e) {
     return r;
 }
 
-ll invp,invm;
-
-ll hashh(vector<ll> &v, ll n) {
-    ll r=0;
-    for(ll i=0;i<n;i++) r = (r+v[i]*pot[i])%mod;
-    return r;
-}
+ll invp;
 
 ll sg(ll a1) {
-    return ((a1*(pot.back()*p%mod-1+mod)%mod)*invp%mod);
+    return ((a1*(pot-1+mod)%mod)*invp%mod);
 }
 
 int main() { _
     ll n,w,ans=0;
     cin >> n >> w;
-    if(w>n) {
-        cout << "0\n";
-        return 0;
-    }
+    
+    vector<ll> A(n),B(w);
+    for(ll &i:A) cin >> i;
+    for(ll &i:B) cin >> i;
+    
+    str_hash<mod> ha(A), hb(B);
+    
+    pot = fexp(b,w);
+    invp = fexp(b-1,mod-2);
 
-    invp = fexp(p-1,mod-2);
-    invm = fexp(p,mod-2);
+    const ll hp = hb(0,w-1);
 
-    pot.push_back(1);
-    for(ll i=1;i<w;i++) pot.push_back(pot.back()*p%mod);
-
-    vector<ll> a(n),b(w);
-    for(ll &i:a) cin >> i;
-    for(ll &i:b) cin >> i;
-
-    const ll hp = hashh(b,w);
-    ll ht = hashh(a,w);
-
-    ll sla = sg(a[0]-b[0]);
-
-    if(hp==((ht-sla+mod)%mod)) ans++;
-
-    for(ll i=0;i+w<n;i++) {
-        ht = (ht-a[i]+mod)%mod;
-        ht = ht*invm%mod;
-        ht = (ht+a[i+w]*pot.back())%mod;
-
-        if(hp==(ht-sg(a[i+1]-b[0])+mod)%mod) ans++;
+    for(ll i=n-1;i-w>=-1;i--) {
+        if(hp== (ha(i-w+1,i)-sg(A[i]-B.back())+mod)%mod) ans++;
     }
 
     cout << ans << '\n';
