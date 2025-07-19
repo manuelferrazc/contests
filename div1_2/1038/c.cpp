4e2ca078dd93518d08567fc1fea047628114e7f7
Manueljunio <manueljunioferraz@hotmail.com>
2025-07-19 20:56:30 -0300

    ... i keep dont knowing

--- div1_2/1038/c.cpp
+++ div1_2/1038/c.cpp
@@ -14,27 +14,48 @@ void solve() {
     int n;
     cin >> n;
 
-    vector<pair<pair<ll,ll>,int>> v(n);
+    vector<pair<pair<ll,ll>,int>> v(n), q1,q2,q3,q4,a1,a2;
     for(int i=0;i<n;i++) {
-        v[i].ss = i;
-
+        v[i].ss = i+1;
         cin >> v[i].x >> v[i].y;
+    }
+
+    if(n==2) {
+        cout << "1 2\n";
+        return;
     }
 
     sort(v.begin(),v.end());
 
-    vector<pair<int,int>> ans;
-
-    ll val = 0;
-    for(int i=0;ans.size()*2<n;i++) {
-        ans.push_back(make_pair(v[i].ss+1,v.back().ss+1));
-        val+=abs(v[i].x-v.back().x);
-        val+=abs(v[i].y-v.back().y);
-        v.pop_back();
+    for(ull i=0,j=v.size()-1;i<j;i++,j--) {
+        a1.push_back(v[i]);
+        a2.push_back(v[j]);
     }
 
-    cout << val << '\n';
-    for(ull i=0;i<ans.size();i++) cout << ans[i].ff << ' ' << ans[i].ss << '\n';
+    auto s = [&](pair<pair<ll,ll>,int> const &a, pair<pair<ll,ll>,int> const &b) -> bool {
+        if(a.y<b.y) return true;
+        if(a.y>b.y) return false;
+        return a.x<b.x;
+    };
+
+    sort(a1.begin(),a1.end(),s);
+    sort(a2.begin(),a2.end(),s);
+
+    for(ull i=0,j=a1.size()-1;i<=j;i++,j--) {
+        q2.push_back(a1[j]);
+        if(i!=j)q3.push_back(a1[i]);
+    }
+
+    for(ull i=0,j=a2.size()-1;i<=j;i++,j--) {
+        if(i!=j) q1.push_back(a2[j]);
+        q4.push_back(a2[i]);
+    }
+    // ll ans=0;
+    for(ull i=0;i<q1.size();i++) cout << q1[i].ss << ' ' << q3[i].ss << '\n';
+    // for(ull i=0;i<q1.size();i++) ans+=abs(q1[i].x-q3[i].x)+abs(q1[i].y-q3[i].y);
+    for(ull i=0;i<q2.size();i++) cout << q2[i].ss << ' ' << q4[i].ss << '\n';
+    // for(ull i=0;i<q2.size();i++) ans+=abs(q2[i].x-q4[i].x)+abs(q2[i].y-q4[i].y);
+    // cout << ans << '\n';
 }
 
 int main() { _
--- div1_2/1038/d.cpp
+++ div1_2/1038/d.cpp
@@ -8,33 +8,14 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-struct st {
-    ll t;
-    ll w;
-    ll v;
-    ll m;
-
-    st(ll _t, ll _w, ll _v, ll _m): t(_t), w(_w), v(_v), m(_m) {}
-};
-
-
-struct ord {
-    bool operator()(const st &a, const st &o) {
-        if(a.t<o.t) return true;
-        if(a.t>o.t) return false;
-        
-        return a.w<o.w;
-    }
-};
-
 void solve() {
-    ll n,m;
+    int n,m;
     cin >> n >> m;
 
-    vector<vector<ll>> adj(n);
+    vector<vector<int>> adj(n);
     
-    for(ll i=0;i<m;i++) {
-        ll a,b;
+    for(int i=0;i<m;i++) {
+        int a,b;
         cin >> a >> b;
         a--;
         b--;
@@ -42,44 +23,26 @@ void solve() {
         adj[b].push_back(a);
     }
 
-    priority_queue<st,vector<st>,ord> pq;
-    pq.push(st(0,0,0,0));
-    vector<vector<ll>> dist(n,vector<ll>(n,LLONG_MAX)), wait(n,vector<ll>(n,LLONG_MAX));
-    dist[0][0] = wait[0][0] = 0;
+    vector<int> act(n,INT_MAX),next(n,INT_MAX);
+    act[0] = 0;
 
-    while(pq.size()) {
-        auto [t,w,v,m] = pq.top();
-        pq.pop();
-
-        if(dist[v][m]<t or (dist[v][m]==t and wait[v][m]<w)) continue;
-
-        for(ull i=0;i<adj[v].size();i++) {
-            ll u = adj[v][(t+i)%adj[v].size()];
-            ll nt = t+i+1, nw = w+i;
-            ll mod = nt%adj[u].size();
-            if(dist[u][mod]>nt or (dist[u][mod]==nt and wait[u][mod]>nw)) {
-                dist[u][mod] = nt;
-                wait[u][mod] = nw;
-                pq.push(st(dist[u][mod],wait[u][mod],u,mod));
-            }
+    for(int t=0;;t++) {
+        if(act.back()!=INT_MAX) {
+            cout << t << ' ' << act.back() << '\n';
+            return;
+        }
+        for(int i=0;i<n;i++) {
+            if(act[i]==INT_MAX) continue;
+            next[i] = min(next[i],act[i]+1);
+            
+            int &x = next[adj[i][t%adj[i].size()]];
+            x = min(x,act[i]);
+        }
+        for(int i=0;i<n;i++) {
+            act[i] = next[i];
+            next[i] = INT_MAX;
         }
     }
-
-    // cout << "costs:\n";
-    // for(int i=0;i<n;i++) cout << dist[i] << ' ' << wait[i] << '\n';
-    pair<ll,ll> ans = make_pair(LLONG_MAX,LLONG_MAX);
-
-    for(ll i=0;i<n;i++) {
-        if(dist.back()[i]<ans.ff) {
-            ans.ff = dist.back()[i];
-            ans.ss = wait.back()[i];
-        } else if(dist.back()[i]==ans.ff and wait.back()[i]<ans.ss) 
-            ans.ss = wait.back()[i];
-    }
-
-    cout << ans.ff << ' ' << ans.ss << '\n';
-    // cout << dist.back() << ' ' << wait.back() << '\n';
-
 }
 
 int main() { _
