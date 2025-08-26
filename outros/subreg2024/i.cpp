a2a50c7a59f33b7ffb929c5f16609feecb7b809d
Manueljunio <manueljunioferraz@hotmail.com>
2025-08-26 01:25:36 -0300

    will run you down, down 'til the dark (?)

--- outros/subreg2024/i.cpp
+++ outros/subreg2024/i.cpp
@@ -17,20 +17,73 @@ ll fexp(ll n, ll e) {
     return e&1 ? r*n%mod : r;
 }
 
-// void fat(vector<ll>)
+vector<int> c(::max+1),where(::max+1,1);
+
+int fatt(vector<int> &fat, int n) {
+    int r=1;
+    while(n>1) {
+        fat.push_back(c[n]);
+        r*=c[n];
+        n = where[n];
+    }
+    return r;
+}
 
 int main() { _
-    vector<int> c(max+1);
     iota(c.begin(),c.end(),0);
-    vector<bool> prime(max+1,false);
-
-    for(int i=2;i<=max;i++) {
+    for(int i=2;i<=::max;i++) {
         if(c[i]!=i) continue;
-        prime[i] = true;
-        for(int j=i+i;j<=max;j+=i) c[j] = i;
+        
+        for(int j=i+i;j<=::max;j+=i) {
+            c[j] = i;
+            if((j/i)%i==0) where[j] = where[j/i];
+            else where[j] = j/i;
+        }
     }
 
+    ll n;
+    cin >> n;
+    vector<int> v(n);
+    vector<int> count(::max+1,0);
+    for(int i:v) {
+        cin >> v[i];
+        count[v[i]]++;
+    }
 
+    vector<int> div(::max+1,0);
+    for(int i=2;i<=::max;i++) {
+        for(int j=i;j<=::max;j+=i) div[i]+=count[j];
+    }
+
+    vector<int> qtd(::max+1);
+    qtd[0] = qtd[1] = 0;
+
+    for(int i=2;i<=::max;i++) {
+        if(c[i]==i) qtd[i] = div[i];
+        else {
+            vector<int> fat;
+            fatt(fat,i);
+            qtd[i] = 0;
+            for(int mask=1;mask<(1<<fat.size());mask++) {
+                int m=0;
+                int x=1;
+                for(int j=0;j<7;j++) if(mask&(1<<j)) {
+                    x*=fat[j];
+                    m++;
+                }
+                if(m&1) qtd[i]+=div[x];
+                else qtd[i]-=div[x];
+            }
+        }
+    }
+
+    int q;
+    cin >> q;
+    while(q--) {
+        int x;
+        cin >> x;
+        cout << fexp(2,n-qtd[x]) << '\n';
+    }
 
     return 0;
 }
