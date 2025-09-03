ee97ecfe13bbe75a7b372c58bb413263df687428
manuelferrazc <manueljunioferraz@hotmail.com>
2025-09-03 17:14:34 -0300

    viraria um quadro (?)

---
+++ cses/string/findingpatterns.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+
+struct SuffAut {
+    vector<int> len;
+    vector<int> link;
+};
+
+int main() { _
+
+    return 0;
+}
