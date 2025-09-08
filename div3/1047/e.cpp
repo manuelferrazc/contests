12894245127a2ed7764e83caf1b920f896b79d9a
Manueljunio <manueljunioferraz@hotmail.com>
2025-09-08 09:51:36 -0300

    hope finally get positive delta today

--- div3/1047/e.cpp
+++ div3/1047/e.cpp
@@ -8,86 +8,52 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-int n,k;
+ll n,k;
 
-void print(const vector<int> &v, char c=' ') {
-    cout << "v: ";
-    for(int i:v) cout << i << ' ';
-    cout << c;
-}
-
-void operation(vector<int> &v) {
+void operation(vector<ll> &v) {
     sort(v.begin(),v.end());
-    // cout << "BEGIN:\n";
-    // print(v,'\n');
-    set<int> nt;
-    for(int i=0;i<=n+1;i++) nt.insert(i);
-    vector<int> qtd(n+1,0);
+    set<ll> nt;
+    for(ll i=0;i<=n+1;i++) nt.insert(i);
+    vector<ll> qtd(n+1,0);
 
-    for(int i:v) {
+    for(ll i:v) {
         qtd[i]++;
         nt.erase(i);
     }
 
-    for(int &i:v) {
+    for(ll &i:v) {
         if(qtd[i]==1) i = min(i,*nt.begin());
         else i = *nt.begin();
     }
-
-    
-    // print(v,'\n');
-
 }
 
-ll soma(const vector<int> &v) {
+ll soma(const vector<ll> &v) {
     ll r=0;
-    for(int i:v) r+=i;
+    for(ll i:v) r+=i;
     return r;
 }
 
 void solve() {
     cin >> n >> k;
 
-    vector<int> v(n);
-    for(int &i:v) cin >> i;
+    vector<ll> v(n);
+    for(ll &i:v) cin >> i;
     
-    ll sa = soma(v);
     operation(v);
+    ll s1 = soma(v);
     
-    if(k==2) operation(v);
-    if(k<=2) cout << soma(v) << '\n'; 
+    if(k==1) cout << s1 << '\n';
     else {
-        map<ll,int> m;
-        m[sa] = 0;
-        m[soma(v)] = 1;
-        for(int i=2;;i++) {
-            operation(v);
-            // cout << "DEBUG ";
-            // print(v);   
-            ll s = soma(v);// << '\n';
-            // cout << "soma: " << soma(v) << '\n';
-            // cout << "variavel: " << s << '\n';
-
-
-            if(m.count(s)) {
-                if((i&1)==(m[s]&1)) {
-                    if((i&1)==(k&1)) cout << s << '\n';
-                    else {
-                        operation(v);
-                        ll s = soma(v);// << '\n';
-                        cout << s << '\n';
-                    }
-                } else cout << s << '\n';
-
-                return;
-            } else m[s] = i;
-
-            if(i==k) {
-                cout << s << '\n';
-                return;
-            }
-        }
+        operation(v);
+        s1 = soma(v);
+        operation(v);
+        ll s2 = soma(v);
+        
+        k--;
+        if(k&1) cout << s1 << '\n';
+        else cout << s2 << '\n';
     }
+        
 }
 
 int main() { _
