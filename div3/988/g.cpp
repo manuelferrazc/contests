8f4ffa2a61ccf0765bbe9d2a786b9cdcc89f487c
Manueljunio <manueljunioferraz@hotmail.com>
2025-09-27 00:14:21 -0300

    will tomorrow have an atcoder?

--- div3/988/g.cpp
+++ div3/988/g.cpp
@@ -11,17 +11,24 @@ typedef unsigned long long ull;
 #define MAX 1'000'000
 const ll mod = 998'244'353;
 
+vector<int> fat(MAX+1,1);
+vector<int> primes_fat(MAX+1,1);
 
+void get_primes(vector<int> &s, int x) {
+    while(x>1) {
+        int f = primes_fat[x];
+        s.push_back(f);
+        while(x%f==0) x/=f;
+    }
+}
 
-int main() { _
-    vector<int> fat(MAX+1,1);
-    vector<int> primes_fat(MAX+1,1);
+int main() { 
 
     for(int i=2;i<=MAX;i++) {
         if(fat[i]!=1) continue;
         fat[i] = i;
         primes_fat[i] = i;
-        for(int j=i+i;j<MAX;j+=i) {
+        for(int j=i+i;j<=MAX;j+=i) {
             fat[j] *= i;
             primes_fat[j] = i;
         }
@@ -31,19 +38,66 @@ int main() { _
     cin >> n;
 
     vector<int> dp(MAX+1,0);
-    int x;
-    cin >> x;
-    x = fat[x];
+    int x,start;
+    cin >> start;
+    start = fat[start];
+    
+    vector<int> f;
 
-    dp[x] = 1;
-    for(int i=1;i<n;i++) {
+    for(int i=1;i<n-1;i++) {
+        f.clear();
+
         cin >> x;
         x = fat[x];
 
-        // vector<int> 
+        get_primes(f,x);
+        ll ans = 0;
+
+        for(int i=1;i<(1<<f.size());i++) {
+            int v = 1,qtd=0;
+            for(int j=0;j<f.size();j++) if(i&(1<<j)) {
+                v*=f[j];
+                qtd++;
+            }
+            if(qtd&1) ans+=dp[v];
+            else ans-=dp[v];
+
+            ans = (ans+mod)%mod;
+        }
+
+        if(gcd(x,start)>1) ans = (ans+1)%mod;
+
+        for(int i=1;i<(1<<f.size());i++) {
+            int v = 1;
+            for(int j=0;j<f.size();j++) if(i&(1<<j)) v*=f[j];
+
+            dp[v] = (dp[v]+ans)%mod;
+        }
     }
 
-    cout << dp[x] << '\n';
+    ll ans = 0;
+
+    int end;
+    cin >> end;
+    end = fat[end];
+
+    f.clear();
+    get_primes(f,end);
+    if(gcd(start,end)>1) ans++;
+
+    for(int i=1;i<(1<<f.size());i++) {
+        int v = 1,qtd=0;
+        for(int j=0;j<f.size();j++) if(i&(1<<j)) {
+            v*=f[j];
+            qtd++;
+        }
+        if(qtd&1) ans+=dp[v];
+        else ans-=dp[v];
+
+        ans = (ans+mod)%mod;
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
