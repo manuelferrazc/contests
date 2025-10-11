5af3061bf41e73594537dd808e45a452423d0783
Manueljunio <manueljunioferraz@hotmail.com>
2025-10-11 01:04:56 -0300

    i dont like elevators

--- cses/dp/elevator.cpp
+++ cses/dp/elevator.cpp
@@ -1,48 +1,52 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+// Auto explicativo
+#pragma GCC optimize("unroll-loops")
+// Vetorizacao
+#pragma GCC target("avx2")
+// Para operacoes com bits
+#pragma GCC target("bmi,bmi2,popcnt,lzcnt")
 
+using namespace std;
+ 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define ff first
 #define ss second
 typedef long long ll;
 typedef unsigned long long ull;
-
+ 
 vector<ll> v;
 vector<int> dp;
 ll x,n,count[1<<20];
-ll b[1<<20];
-
-
+ 
+ 
 int ddp(int m) {
     if(dp[m]!=INT_MAX) return dp[m];
-
-    for(int i=m;i&(1<<b[m]);i=(i-1)&m) {
+ 
+    int sla=28;
+    for(;(m&(1<<sla))==0;sla--) {}
+    for(int i=m;i&(1<<sla);i=(i-1)&m) {
         if(::count[i]<=x) dp[m] = min(dp[m],1+ddp(m-i));
     }
-
+ 
     return dp[m];
 }
-
-int main() { 
+ 
+int main() { _
     cin >> n >> x;
     v.resize(n);
     dp.resize(1<<n,INT_MAX);
     dp[0] = 0;
     for(ll &i:v) cin >> i;
-
+ 
     for(int i=0;i<(1<<n);i++) {
         ::count[i] = 0;
-        b[i] = 0;
         for(int j=0;j<n;j++) {
-            if(i&(1<<j)) {
-                b[i] = j;
-                ::count[i]+=v[j];
-            }
+            if(i&(1<<j)) ::count[i]+=v[j];
         }
     }
-
+ 
     cout << ddp((1<<n)-1) << '\n';
-
+ 
     return 0;
 }
--- div2/1057/d.cpp
+++ div2/1057/d.cpp
@@ -8,79 +8,49 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
+ll med(ll a, ll b, ll c) {
+    if(a<=b and b<=c) return c-a;
+    if(c<=b and b<=a) return a-c;
+
+    if(a<=c and c<=b) return b-a;
+    if(b<=c and c<=a) return a-b;
+
+    if(b<=a and a<=c) return c-b;
+    return b-c;
+}
+
+ll count(ll *v, int n) {
+    ll dp[n+1][4];
+    
+    dp[0][2] = 0;
+    dp[0][3] = 0;
+    
+    dp[1][2] = 1e16;
+    dp[1][3] = 1e16;
+
+    dp[2][2] = abs(v[2]-v[1]);
+    dp[2][3] = 1e16;
+    for(int i=3;i<=n;i++) {
+        dp[i][2] = abs(v[i]-v[i-1])+min(dp[i-2][2],dp[i-2][3]);
+        dp[i][3] = med(v[i],v[i-1],v[i-2])+min(dp[i-3][2],dp[i-3][3]);
+    }
+
+    return min(dp[n][2],dp[n][3]);
+}
+
 void solve() {
     int n;
     cin >> n;
 
-    vector<ll> v(n);
-    for(ll &i:v) cin >> i;
+    ll v[n+3];
+    for(int i=1;i<=n;i++) cin >> v[i];
+    v[n+1] = v[1];
+    v[n+2] = v[2];
 
-    // dp[i][0] -> batendo com o anterior
-    // dp[i][1] -> nao batendo com o anterior
-    
-    // vou mudar a0 para a1, logo n tem q preocupar com a0
-    vector<ll[2]> dp(n);
-    vector<ll[2]> val(n);
-    dp[1][0] = abs(v[1]-v[0]);
-    dp[1][1] = 1e16;
-    val[0][0] = val[0][1] = v[1];
-    val[1][0] = v[1];
-    val[1][1] = v[1];
-    
+    ll ans = count(v,n);
 
-    for(int i=2;i<n;i++) {
-        dp[i][0] = min(dp[i-1][0]+abs(v[i]-val[i-1][0]),dp[i-1][1]+abs(v[i]-v[i-1]));
-        // dp[i][0]+=abs(v[i]-v[i-1]);
-        if(dp[i][0]==dp[i-1][0]+abs(v[i]-v[i-1])) val[i][0] = val[i-1][0];
-        else val[i][0] = v[i-1];
-
-        dp[i][1] = min({dp[i-1][0],// ok
-                        dp[i-2][1]+abs(v[i-2]-v[i-1]),//ok
-                        dp[i-2][0]+abs(val[i-2][0]-v[i-1])
-                    });
-
-        val[i][1] = v[i-1];
-    }
-
-    // ll ans = min({dp[n-2][0]+abs(v[n-2]-v[n-1]),
-    //               dp[n-2][0]+abs(v[n-1]-v[1]),
-    //               dp[n-2][1]+abs(v[n-1]-v[n-2])});
-    ll ans = min(dp[n-1][0],dp[n-2][0]+abs(v[n-1]-v[1]));
-
-    for(int i=1;i<n;i++) cout << dp[i][0] << ' ';
-    cout << '\n';
-    for(int i=1;i<n;i++) cout << dp[i][1] << ' ';
-    cout << '\n';
-    
-    // agora nao vou mudar a0 para a1, entao tem q acertar no final
-    dp[1][0] = 1e16;
-    dp[1][1] = 0;
-    val[1][0] = val[1][1] = v[1];
-    val[0][0] = val[0][1] = v[0];
-
-
-    for(int i=2;i<n;i++) {
-        dp[i][0] = min(dp[i-1][0]+abs(v[i]-val[i-1][0]),dp[i-1][1]+abs(v[i]-v[i-1]));
-        // dp[i][0]+=abs(v[i]-v[i-1]);
-        if(dp[i][0]==dp[i-1][0]+abs(v[i]-v[i-1])) val[i][0] = val[i-1][0];
-        else val[i][0] = v[i-1];
-
-        dp[i][1] = min({dp[i-1][0],// ok
-                        dp[i-2][1]+abs(v[i-2]-v[i-1]),//ok
-                        dp[i-2][0]+abs(val[i-2][0]-v[i-1])
-                    });
-
-        val[i][1] = v[i-1];
-    }
-
-    cout << '\n';
-
-    for(int i=1;i<n;i++) cout << dp[i][0] << ' ';
-    cout << '\n';
-    for(int i=1;i<n;i++) cout << dp[i][1] << ' ';
-    cout << '\n';
-
-    ans = min({ans,dp[n-2][0]+abs(v[0]-v[n-1]),dp[n-1][0]+abs(v[0]-val[n-1][0])});
+    ans = min(ans,count(v+1,n));
+    ans = min(ans,count(v+2,n));
 
     cout << ans << '\n';
 }
