09a00d6394bcbf285dcefe81ae707910adad35bd
manuelferrazc <manueljunioferraz@hotmail.com>
2025-10-23 23:43:51 -0300

    a little while

--- pc2/spraguegrundy/marbles.cpp
+++ pc2/spraguegrundy/marbles.cpp
@@ -15,19 +15,19 @@ int mex(vector<bool> &v) {
 int main() { 
     int dp[101][101];
 
-    for(int i=0;i<=100;i++) {
-        for(int j=0;j<=100;j++) {
+    for(int i=1;i<=100;i++) dp[i][i] = dp[i][0] = dp[0][i] = INT_MAX;
+
+    for(int i=1;i<=100;i++) {
+        for(int j=1;j<=100;j++) {
+            if(i==j) continue;
             int mx = max(i,j);
             int mi = min(i,j);
             vector<bool> v(302,false);
 
-            for(int u=1;u<=mx;u++) {
-                if(mi-u>=0) {
-                    v[dp[i][j-u]] = true;
-                    v[dp[i-u][j-u]] = true;
-                    v[dp[i-u][j]] = true;
-                } else if(i-u>=0) v[dp[i-u][j]] = true;
-                else v[dp[i][j-u]] = true;
+            for(int u=1;u<mx;u++) {
+                if(mi-u>0) v[dp[i-u][j-u]] = true;
+                if(i-u>0 and i-u!=j) v[dp[i-u][j]] = true;
+                if(j-u>0 and j-u!=i) v[dp[i][j-u]] = true;
             }
 
             dp[i][j] = mex(v);
