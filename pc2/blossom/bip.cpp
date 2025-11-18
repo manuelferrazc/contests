20d1095a2b498a0e05ec1b802d935602eea1623d
manuelferrazc <manueljunioferraz@hotmail.com>
2025-11-18 15:51:58 -0300

    duck typing

---
+++ pc2/blossom/bip.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+
+const int INF = 0x3f3f3f3f;
+
+struct dinitz {
+	const bool scaling = false; // com scaling -> O(nm log(MAXCAP)),
+	int lim;                    // com constante alta
+	struct edge {
+		int to, cap, rev, flow;
+		bool res;
+		edge(int to_, int cap_, int rev_, bool res_)
+			: to(to_), cap(cap_), rev(rev_), flow(0), res(res_) {}
+	};
+
+	vector<vector<edge>> g;
+	vector<int> lev, beg;
+	ll F;
+	dinitz(int n) : g(n), F(0) {}
+
+	void add(int a, int b, int c) {
+		g[a].emplace_back(b, c, g[b].size(), false);
+		g[b].emplace_back(a, 0, g[a].size()-1, true);
+	}
+	bool bfs(int s, int t) {
+		lev = vector<int>(g.size(), -1); lev[s] = 0;
+		beg = vector<int>(g.size(), 0);
+		queue<int> q; q.push(s);
+		while (q.size()) {
+			int u = q.front(); q.pop();
+			for (auto& i : g[u]) {
+				if (lev[i.to] != -1 or (i.flow == i.cap)) continue;
+				if (scaling and i.cap - i.flow < lim) continue;
+				lev[i.to] = lev[u] + 1;
+				q.push(i.to);
+			}
+		}
+		return lev[t] != -1;
+	}
+	int dfs(int v, int s, int f = INF) {
+		if (!f or v == s) return f;
+		for (int& i = beg[v]; i < g[v].size(); i++) {
+			auto& e = g[v][i];
+			if (lev[e.to] != lev[v] + 1) continue;
+			int foi = dfs(e.to, s, min(f, e.cap - e.flow));
+			if (!foi) continue;
+			e.flow += foi, g[e.to][e.rev].flow -= foi;
+			return foi;
+		}
+		return 0;
+	}
+	ll max_flow(int s, int t) {
+		for (lim = scaling ? (1<<30) : 1; lim; lim /= 2)
+			while (bfs(s, t)) while (int ff = dfs(s, t)) F += ff;
+		return F;
+	}
+};
+
+int main() { _
+    int n,m;
+    cin >> n >> m;
+
+    dinitz d(n+m+2);
+    for(int i=1;i<=n;i++) d.add(0,i,1);
+    for(int i=1;i<=m;i++) d.add(n+i,n+m+1,1);
+
+    for(int i=1;i<=n;i++) {
+        int x;
+        cin >> x;
+
+        while(x) {
+            d.add(i,n+x,1);
+            cin >> x;
+        }
+    }
+
+    cout << d.max_flow(0,n+m+1) << '\n';
+
+    for(int i=1;i<=n;i++) {
+        for(auto [u,fds,fds2,f,fds3]:d.g[i]) {
+            if(u>n and f) cout << i << ' ' << u-n << '\n';
+        }
+    }
+    
+    return 0;
+}
