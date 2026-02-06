e06c491ab22c71210be84654e56b1f9aa7c65167
manuelferrazc <manueljunioferraz@hotmail.com>
2026-02-06 18:55:38 -0300

    and more upsolving

--- summer/02/i.cpp
+++ summer/02/i.cpp
@@ -14,7 +14,6 @@ int main() { _
     cin >> n >> t;
 
     map<int,vector<int>> adj[n];
-    vector<int> v[n];
 
     for(int i=0;i<t;i++) {
         int q;
@@ -28,15 +27,7 @@ int main() { _
 
             adj[a][i].push_back(b);
             adj[b][i].push_back(a);
-
-            if(v[a].empty() or v[a].back()!=i) v[a].push_back(i);
         }
-    }
-
-    for(int i=0;i<n;i++) {
-        cout << i << ": ";
-        for(auto a:v[i]) cout << a << ' ';
-        cout << '\n';
     }
 
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
@@ -49,38 +40,46 @@ int main() { _
 
     int k;
     cin >> k;
-    int m[k];
+    map<int,vector<int>> m;
     for(int i=0;i<k;i++) {
-        cin >> m[i];
-        m[i]--;
+        int x;
+        cin >> x;
+        m[x-1].push_back(i);
     }
 
+    // int ans = INT_MAX;
     while(not pq.empty()) {
         auto [tr,u] = pq.top();
         pq.pop();
-
+        
         if(dist[u]<tr) continue;
+        
+        /*
+         * ff -> momento do tempo que a aresta tá viva
+         * ss -> pra quais vertices essas arestas vão
+         */
+        for(auto it = adj[u].begin();it!=adj[u].end();it++) {
+            // se a maquina nunca leva pra esse momento, ele é inútil 
+            if(m.count(it->ff)==0) continue;
 
-        auto it = lower_bound(v[u].begin(),v[u].end(),tr);
+            // it2 -> memor número de viagens que preciso pra chegar em it->ff, dado que estou na tr-esima viagem
+            auto it2 = lower_bound(m[it->ff].begin(),m[it->ff].end(),tr);
+            if(it2==m[it->ff].end()) continue;
 
-        for(;it!=v[u].end();it++) {
-            cout << u << ' ' << m[*it] << ": \n";
-            for(auto u2:adj[u][m[*it]]) {
-                cout << u2 << ' ';
-                if(dist[u2]>*it+1) {
-                    cout << "sim\n";
-                    dist[u2] = *it+1;
-                    pq.push(pair(*it+1,u2));
+            for(auto v:it->ss) {
+                if(dist[v]>*it2+1) {
+                    dist[v] = *it2+1;
+
+                    // if(v==n-1) ans = min(ans,tr+1);
+                    pq.push(pair(dist[v],v));
                 }
-                else cout << "não\n";
             }
         }
+
     }
 
-    // for(int i=0;i<n;i++) cout << i << ' ' << dist[i] << '\n';
-
     if(dist[n-1]==INT_MAX) cout << -1 << '\n';
-    else cout << dist[n-1]-1 << '\n';
+    else cout << dist[n-1] << '\n';
 
     return 0;
 }
