409e3eeff79fa015003334104e48fd9edbb7642a
manuelferrazc <manueljunioferraz@hotmail.com>
2026-02-15 00:01:04 -0300

    baby, like this is perfection (?)

--- cses/rangequeries/iaq.cpp
+++ cses/rangequeries/iaq.cpp
@@ -8,106 +8,106 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-struct SegTree {
-    ll *sum;
-    ll *lz;
-    ll *lzh;
+#define MAX 200'000
+#define MAX2 18
+
+namespace sparse {
     int n;
+	pair<int,int> m[MAX2][2*MAX], v[2*MAX];
+	pair<int,int> op(pair<int,int> a, pair<int,int> b) { return max(a, b); }
+	void build(int n2, pair<int,int>* v2) {
+		n = n2;
+		for (int i = 0; i < n; i++) v[i] = v2[i];
+		while (n&(n-1)) n++;
+		for (int j = 0; (1<<j) < n; j++) {
+			int len = 1<<j;
+			for (int c = len; c < n; c += 2*len) {
+				m[j][c] = v[c], m[j][c-1] = v[c-1];
+				for (int i = c+1; i <  c+len; i++) m[j][i] = op(m[j][i-1], v[i]);
+				for (int i = c-2; i >= c-len; i--) m[j][i] = op(v[i], m[j][i+1]);
+			}
+		}
+	}
+	pair<int,int> query(int l, int r) {
+		if (l == r) return v[l];
+		int j = __builtin_clz(1) - __builtin_clz(l^r);
+		return op(m[j][l], m[j][r]);
+	}
+}
 
-    ll build(int pos, int l, int r, int *v) {
-        lz[pos] = lzh[pos] = 0;
-        if(l==r) return st[pos] = v[l];
+ll get(ll *sp, int a, int b) {
+    if(a) return sp[b]-sp[a-1];
+    return sp[b];
+}
 
-        int m = (l+r)>>1, ls = 2*pos+1;
-        return st[pos] = build(ls,l,m,v)+build(ls+1,m+1,r,v);
-    }
+void make_sp(int *v, int n, ll *sp) {
+    sp[0] = v[0];
+    for(int i=1;i<n;i++) sp[i] = v[i]+sp[i-1];
+}
 
-    SegTree(int _n, int *v) {
-        n = _n;
-        st = new ll[4*n];
-        lz = new ll[4*n];
-        lzh = new ll[4*n];
+void make_next_big(int *v, int n, int *next) {
+    stack<int> s;
 
-        build(0,0,n-1,v);
-    }
-
-    ~SegTree() {
-        delete[] st;
-        delete[] lz;
-        delete[] lzh;
-    }
-
-    void propag(int pos, int l, int r) {
-        ll sz = r-l+1;
-        st[pos] += sz*lz[pos] + lzh[pos]*(sz+1)*sz/2;
-
-        if(sz>1) {
-            int ls = 2*pos+1, m = (l+r)>>1;
-            lzh[ls] += lzh[pos];
-            lzh[ls+1] += lzh[pos];
-
-            lz[ls] += lz[pos];
-            lz[ls+1] += (m-l+1)*lzh[pos]+lz[pos];
+    for(int i=0;i<n;i++) {
+        while(s.size() and v[s.top()]<=v[i]) {
+            next[s.top()] = i;
+            s.pop();
         }
 
-        lz[pos] = lzh[pos] = 0;
+        s.push(i);
     }
 
-    ll get(int pos, int l, int r, int lq, int rq) {
-        if(rq<l or r<lq) return 0;
-
-        propag(pos,l,r);
-        if(lq<=l and r<=rq) return st[pos];
-        
-        int m = (l+r)>>1, ls = 2*pos+1;
-        return get(ls,l,m,lq,rq)+get(ls+1,m+1,r,lq,rq);
+    while(s.size()) {
+        next[s.top()] = -1;
+        s.pop();
     }
+}
 
-    ll get(int l, int r) {
-        return get(0,0,n-1,l,r);
+void make_suf(int *v, ll *sp, int *next, ll *suf, int n) {
+    for(ll i=n-1;i>=0;i--) {
+        if(next[i]==-1) suf[i] = (n-i)*v[i] - get(sp,i,n-1);
+        else suf[i] = (next[i]-i)*v[i] - get(sp,i,next[i]-1) + suf[next[i]];
     }
-
-    ll add(int pos, int l, int r, int lq, int rq, ll qtd=0) {
-        propag(pos,l,r);
-        if(rq<l or r<lq) return st[pos];
-        if(lq<=l and r<=rq) {
-            lzh[pos]++;
-            lz[pos]+=qtd;
-            propag(pos,l,r);
-            return st[pos];
-        }
-
-        int m = (l+r)>>1, ls = 2*pos+1;
-
-        ll nqtd=0;
-
-        if(lq<=m and m<=rq) nqtd = m-max(lq,l)+1;
-        return st[pos] = add(ls,l,m,lq,rq,qtd)+add(ls+1,m+1,r,lq,rq,qtd+nqtd);
-    }
-
-    void add(int l, int r) {
-        add(0,0,n-1,l,r);
-    }
-};
+}
 
 int main() { _
-    int n,q;
+    ll n,q;
     cin >> n >> q;
 
     int v[n];
-    for(int i=0;i<n;i++) cin >> i[v];
+    for(int i=0;i<n;i++) cin >> v[i];
+    
+    pair<int,int> v2[n];
+    for(int i=0;i<n;i++) {
+        v2[i].ff = v[i];
+        v2[i].ss = i;
+    }
+    sparse::build(n,v2);
 
-    SegTree st(n,v);
+    ll sp[n];
+    make_sp(v,n,sp);
 
-    int op,a,b;
+    int next[n];
+    make_next_big(v,n,next);
+
+    ll suf[n];
+    make_suf(v,sp,next,suf,n);
 
     while(q--) {
-        cin >> op >> a >> b;
-        a--;
-        b--;
+        int l,r;
+        cin >> l >> r;
+        l--;
+        r--;
 
-        if(op==1) st.add(a,b);
-        else cout << st.get(a,b) << '\n';
+        if(r==n-1) cout << suf[l] << '\n';
+        else {
+            auto [mx,id] = sparse::query(l,r);
+
+            if(next[id]==-1) cout << suf[l] - (mx*(n-r-1ll) - get(sp,r+1,n-1)) << '\n';
+            else 
+                cout << suf[l] - suf[next[id]] - (mx*(next[id]-r-1ll) - get(sp,r+1,next[id]-1)) << '\n';
+            
+        }
     }
     
     return 0;
